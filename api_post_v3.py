02e0c010272422234c9740568439d3a82140166e
xuzhigui <xuzhigui1991@163.com>
2019-01-24 15:53:42 +0800

    changes

--- api_post_v3.py
+++ api_post_v3.py
@@ -42,7 +42,7 @@ canonical_request = (http_request_method + "\n" +
                      canonical_headers + "\n" +
                      signed_headers + "\n" +
                      hashed_request_payload)
-print(canonical_request)
+#print(canonical_request)
 
 # ************* 步骤 2：拼接待签名字符串 *************
 credential_scope = date + "/" + service + "/" + "tc3_request"
@@ -51,7 +51,7 @@ string_to_sign = (algorithm + "\n" +
                   str(timestamp) + "\n" +
                   credential_scope + "\n" +
                   hashed_canonical_request)
-print(string_to_sign)
+#print(string_to_sign)
 
 # ************* 步骤 3：计算签名 *************
 # 计算签名摘要函数
@@ -61,14 +61,14 @@ secret_date = sign(("TC3" + secret_key).encode("utf-8"), date)
 secret_service = sign(secret_date, service)
 secret_signing = sign(secret_service, "tc3_request")
 signature = hmac.new(secret_signing, string_to_sign.encode("utf-8"), hashlib.sha256).hexdigest()
-print(signature)
+#print(signature)
 
 # ************* 步骤 4：拼接 Authorization *************
 authorization = (algorithm + " " +
                  "Credential=" + secret_id + "/" + credential_scope + ", " +
                  "SignedHeaders=" + signed_headers + ", " +
                  "Signature=" + signature)
-print(authorization)
+#print(authorization)
 
 # 公共参数添加到请求头部
 headers = {
@@ -80,9 +80,11 @@ headers = {
     "X-TC-Version": version,
     "X-TC-Region": region,
 }
-print(headers)
+print(json.dumps(headers, indent=4))
+print("==========================================")
 
-print('payload is', payload)
+#print('payload is', payload)
 r = requests.post(endpoint, headers=headers, data=payload)
 print(r.text)
-print(r.headers)
+print("==========================================")
+print(json.dumps(dict(r.headers),indent=4))
