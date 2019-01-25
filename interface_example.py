cd6f679412e5b70472dae15f498adcc69c8e1b83
xuzhigui <xuzhigui1991@163.com>
2019-01-25 18:39:38 +0800

    changes

--- interface_example.py
+++ interface_example.py
@@ -42,19 +42,19 @@ def api_call(key, args={}):
 
 
 if __name__ == "__main__":
-#    # 查询北京的服务器
+#    # 查询北京的服务器 https://cloud.tencent.com/document/api/213/15728
 #    c = api_call('cvm.list', {"region": "ap-beijing"})
 #    print(c.text)
 #
 #
-#    # 查询所有区域
+#    # 查询所有区域 https://cloud.tencent.com/document/api/213/15708
 #    #d = api_call('area.list', {"region": ""})
 #    d = api_call('area.list')
 #    j = json.loads(d.text)
 #    print(json.dumps(j,indent=4, ensure_ascii=False))
 
 
-    # 查询所有用户
+    # 查询所有用户 https://cloud.tencent.com/document/api/598/15297
     h = api_call('user.list')
     print(h.text)
     k = json.loads(h.text)
