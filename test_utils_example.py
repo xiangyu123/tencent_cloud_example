30482898e287ce562105f9e6e40e4a99be074e55
xuzhigui <xuzhigui1991@163.com>
2019-01-28 18:41:40 +0800

    changes

--- test_utils_example.py
+++ test_utils_example.py
@@ -5,7 +5,9 @@ import json
 from utils.TencentCommonService import TencentCommonService
 
 cvm_instance_example = TencentCommonService(url="https://cvm.tencentcloudapi.com", service="cvm", region="ap-beijing", action="DescribeInstances")
-#r = cvm_instance_example.execute_action({"Limit": 10, "Offset": 0})
-r = cvm_instance_example.execute_action()
+
+# 以下两个都可以执行
+r = cvm_instance_example.execute_action({"Limit": 10, "Offset": 0})
+#r = cvm_instance_example.execute_action()
 print(r.text)
 print(json.dumps(dict(r.headers),indent=4))
