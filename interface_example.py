47afa1c8dc40bae5acc55ddd5af0fa7d96a53b1e
xuzhigui <xuzhigui1991@163.com>
2019-01-29 12:13:19 +0800

    changes

--- interface_example.py
+++ interface_example.py
@@ -68,16 +68,16 @@ def api_call(key, args={}):
 
 
 if __name__ == "__main__":
-    # # 查询北京的服务器 https://cloud.tencent.com/document/api/213/15728
-    # c = api_call('cvm.list', {"region": "ap-beijing"})
-    # print(c.text)
+    # 查询北京的服务器 https://cloud.tencent.com/document/api/213/15728
+    c = api_call('cvm.list', {"region": "ap-beijing"})
+    print(c.text)
 
 
-    # # 查询所有区域 https://cloud.tencent.com/document/api/213/15708
-    # d = api_call('area.list', {"region": ""})
-    # d = api_call('area.list')
-    # j = json.loads(d.text)
-    # print(json.dumps(j,indent=4, ensure_ascii=False))
+    # 查询所有区域 https://cloud.tencent.com/document/api/213/15708
+    d = api_call('area.list', {"region": ""})
+    d = api_call('area.list')
+    j = json.loads(d.text)
+    print(json.dumps(j,indent=4, ensure_ascii=False))
 
 
     # 查询所有用户 https://cloud.tencent.com/document/api/598/15297
@@ -85,15 +85,15 @@ if __name__ == "__main__":
     k = json.loads(h.text)
     print(json.dumps(k, indent=4, ensure_ascii=False))
 
-    # # 查询所有策略 https://cloud.tencent.com/document/api/598/15426
-    # l = api_call('policy.list')
-    # m = json.loads(l.text)
-    # print(json.dumps(m, indent=4, ensure_ascii=False))
+    # 查询所有策略 https://cloud.tencent.com/document/api/598/15426
+    l = api_call('policy.list')
+    m = json.loads(l.text)
+    print(json.dumps(m, indent=4, ensure_ascii=False))
 
-    # # 查询所有策略关联的实体 https://cloud.tencent.com/document/api/598/15425
-    # o = api_call('policy.listentitiesforpolicy')
-    # p = json.loads(o.text)
-    # print(json.dumps(p, indent=4, ensure_ascii=False))
+    # 查询所有策略关联的实体 https://cloud.tencent.com/document/api/598/15425
+    o = api_call('policy.listentitiesforpolicy')
+    p = json.loads(o.text)
+    print(json.dumps(p, indent=4, ensure_ascii=False))
 
     # 查询某个指定子账号关联的所有策略 https://cloud.tencent.com/document/api/598/15423
     o = api_call('policy.listattacheduserpolicies')
